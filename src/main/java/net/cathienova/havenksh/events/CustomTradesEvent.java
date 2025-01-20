0024aafc6a4d770516d1097021c08e77479359f4
Cathienna <Cathienna69@gmail.com>
2025-01-21 00:53:53 +0100

    Fixed the Cobblemaster Villager, now takes the job.

--- src/main/java/net/cathienova/havenksh/events/CustomTradesEvent.java
+++ src/main/java/net/cathienova/havenksh/events/CustomTradesEvent.java
@@ -24,77 +24,77 @@ public class CustomTradesEvent
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent event)
     {
-        if (event.getType() == ModVillagers.COBBLEMASTER.get())
+        if (event.getType() == ModVillagers.COBBLEMASTER.value())
         {
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
 
             // Check if the list at the key exists, if not, create a new list
-            trades.computeIfAbsent(1, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.STONE, 16), // item paid
                     new ItemStack(Items.EMERALD, 1),
                     12, 2, 0.02f));
 
-            trades.computeIfAbsent(1, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(1).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.GRAVEL, 8), // item paid
                     new ItemStack(Items.EMERALD, 1),
                     12, 2, 0.02f));
 
-            trades.computeIfAbsent(2, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(2).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.STONE, 4),
                     12, 3, 0.02f));
 
-            trades.computeIfAbsent(2, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(2).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.COBBLED_DEEPSLATE, 4),
                     12, 3, 0.02f));
 
-            trades.computeIfAbsent(3, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.GRANITE, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(3, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.DRIPSTONE_BLOCK, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(4, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(3).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.DIORITE, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(4, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(4).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.TUFF, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(4, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(4).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.CALCITE, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(4, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(4).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.BASALT, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(4, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(4).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 3), // item paid
                     new ItemStack(Items.ANDESITE, 4),
                     12, 4, 0.02f));
 
-            trades.computeIfAbsent(5, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(5).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 2), // item paid
                     new ItemStack(Items.DEEPSLATE, 4),
                     12, 8, 0.02f));
 
-            trades.computeIfAbsent(5, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(5).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 3), // item paid
                     new ItemStack(ModBlocks.scorched_stone.get(), 4),
                     16, 8, 0.02f));
 
-            trades.computeIfAbsent(5, k -> new ArrayList<>()).add((pTrader, pRandom) -> new MerchantOffer(
+            trades.get(5).add((entity, randomSource) -> new MerchantOffer(
                     new ItemCost(Items.EMERALD, 32), // item paid
                     new ItemStack(ModItems.raw_havenite.get(), 2),
                     4, 8, 0.02f));
--- src/main/java/net/cathienova/havenksh/util/ModVillagers.java
+++ src/main/java/net/cathienova/havenksh/util/ModVillagers.java
@@ -3,6 +3,7 @@ package net.cathienova.havenksh.util;
 import com.google.common.collect.ImmutableSet;
 import net.cathienova.havenksh.HavenKSH;
 import net.cathienova.havenksh.block.ModBlocks;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
@@ -22,14 +23,14 @@ public class ModVillagers
     public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS =
             DeferredRegister.create(BuiltInRegistries.VILLAGER_PROFESSION, HavenKSH.MOD_ID);
 
-    public static final Supplier<PoiType> COBBLEMASTER_POI = POI_TYPES.register("cobblemaster_poi",
+    public static final Holder<PoiType> COBBLEMASTER_POI = POI_TYPES.register("cobblemaster_poi",
             () -> new PoiType(ImmutableSet.copyOf(ModBlocks.wooden_cobble_gen.get().getStateDefinition().getPossibleStates()),
-                    1, 2));
+                    1, 1));
 
-    public static final Supplier<VillagerProfession> COBBLEMASTER =
-            VILLAGER_PROFESSIONS.register("cobblemaster", () -> new VillagerProfession("cobblemaster",
-                    holder -> holder == COBBLEMASTER_POI, holder -> holder == COBBLEMASTER_POI,
-                    ImmutableSet.of(), ImmutableSet.of(), SoundEvents.VILLAGER_WORK_ARMORER));
+    public static final Holder<VillagerProfession> COBBLEMASTER = VILLAGER_PROFESSIONS.register("cobblemaster",
+            () -> new VillagerProfession("cobblemaster", holder -> holder.value() == COBBLEMASTER_POI.value(),
+                    poiTypeHolder -> poiTypeHolder.value() == COBBLEMASTER_POI.value(), ImmutableSet.of(), ImmutableSet.of(),
+                    SoundEvents.VILLAGER_WORK_MASON));
 
     public static void register(IEventBus eventBus)
     {
