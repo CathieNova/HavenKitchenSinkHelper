5b129f28880664aa2754c95fa00b094bb87f96f9
Cathienna <Cathienna69@gmail.com>
2025-01-23 03:18:55 +0100

    Added Config for colors of Highlights

--- src/main/java/net/cathienova/havenksh/config/CommonConfig.java
+++ src/main/java/net/cathienova/havenksh/config/CommonConfig.java
@@ -10,7 +10,15 @@ public class CommonConfig {
     public final ModConfigSpec.IntValue dragonScaleDropAmount;
     public final ModConfigSpec.IntValue wateringCanCooldown;
     public final ModConfigSpec.BooleanValue hammerDurability;
+    public final ModConfigSpec.DoubleValue hammer_highlight_red;
+    public final ModConfigSpec.DoubleValue hammer_highlight_green;
+    public final ModConfigSpec.DoubleValue hammer_highlight_blue;
+    public final ModConfigSpec.DoubleValue hammer_highlight_alpha;
     public final ModConfigSpec.BooleanValue excavatorDurability;
+    public final ModConfigSpec.DoubleValue excavator_highlight_red;
+    public final ModConfigSpec.DoubleValue excavator_highlight_green;
+    public final ModConfigSpec.DoubleValue excavator_highlight_blue;
+    public final ModConfigSpec.DoubleValue excavator_highlight_alpha;
 
     public CommonConfig(ModConfigSpec.Builder builder) {
         builder.comment("Mending Necklace").push("mendingNecklace").build();
@@ -31,9 +39,22 @@ public class CommonConfig {
         wateringCanCooldown = builder.comment("The cooldown in ticks for the watering can.").defineInRange("wateringCanCooldown", 10, 1, 1000);
         builder.pop();
 
-        builder.comment("Hammer And Excavator Durability").push("hammerDurability").build();
+        builder.comment("Hammers").push("hammers").build();
         hammerDurability = builder.comment("If enabled, the hammers only takes 1 durability instead of the amount it breaks.").define("hammerDurability", true);
+        builder.comment("Highlight color for the hammer").push("hammer_highlight").build();
+        hammer_highlight_red = builder.comment("The red value of the highlight color.").defineInRange("hammer_highlight_red", 1.0f, 0.0f, 1.0f);
+        hammer_highlight_green = builder.comment("The green value of the highlight color.").defineInRange("hammer_highlight_green", 0.0f, 0.0f, 1.0f);
+        hammer_highlight_blue = builder.comment("The blue value of the highlight color.").defineInRange("hammer_highlight_blue", 1.0f, 0.0f, 1.0f);
+        hammer_highlight_alpha = builder.comment("The alpha value of the highlight color.").defineInRange("hammer_highlight_alpha", 0.4f, 0.0f, 1.0f);
+        builder.pop(2);
+
+        builder.comment("Excavators").push("excavators").build();
         excavatorDurability = builder.comment("If enabled, the excavators only takes 1 durability instead of the amount it breaks.").define("excavatorDurability", true);
-        builder.pop();
+        builder.comment("Highlight color for the excavator").push("excavator_highlight").build();
+        excavator_highlight_red = builder.comment("The red value of the highlight color.").defineInRange("excavator_highlight_red", 1.0f, 0.0f, 1.0f);
+        excavator_highlight_green = builder.comment("The green value of the highlight color.").defineInRange("excavator_highlight_green", 0.0f, 0.0f, 1.0f);
+        excavator_highlight_blue = builder.comment("The blue value of the highlight color.").defineInRange("excavator_highlight_blue", 1.0f, 0.0f, 1.0f);
+        excavator_highlight_alpha = builder.comment("The alpha value of the highlight color.").defineInRange("excavator_highlight_alpha", 0.4f, 0.0f, 1.0f);
+        builder.pop(2);
     }
 }
--- src/main/java/net/cathienova/havenksh/config/HavenConfig.java
+++ src/main/java/net/cathienova/havenksh/config/HavenConfig.java
@@ -12,7 +12,15 @@ public class HavenConfig
     public static int dragonScaleDropAmount;
     public static int wateringCanCooldown;
     public static boolean hammerDurability;
+    public static double hammer_highlight_red;
+    public static double hammer_highlight_green;
+    public static double hammer_highlight_blue;
+    public static double hammer_highlight_alpha;
     public static boolean excavatorDurability;
+    public static double excavator_highlight_red;
+    public static double excavator_highlight_green;
+    public static double excavator_highlight_blue;
+    public static double excavator_highlight_alpha;
 
     public static void bake(ModConfig config)
     {
@@ -23,6 +31,14 @@ public class HavenConfig
         dragonScaleDropAmount = HavenKSH.c_config.dragonScaleDropAmount.get();
         wateringCanCooldown = HavenKSH.c_config.wateringCanCooldown.get();
         hammerDurability = HavenKSH.c_config.hammerDurability.get();
+        hammer_highlight_red = HavenKSH.c_config.hammer_highlight_red.get();
+        hammer_highlight_green = HavenKSH.c_config.hammer_highlight_green.get();
+        hammer_highlight_blue = HavenKSH.c_config.hammer_highlight_blue.get();
+        hammer_highlight_alpha = HavenKSH.c_config.hammer_highlight_alpha.get();
         excavatorDurability = HavenKSH.c_config.excavatorDurability.get();
+        excavator_highlight_red = HavenKSH.c_config.excavator_highlight_red.get();
+        excavator_highlight_green = HavenKSH.c_config.excavator_highlight_green.get();
+        excavator_highlight_blue = HavenKSH.c_config.excavator_highlight_blue.get();
+        excavator_highlight_alpha = HavenKSH.c_config.excavator_highlight_alpha.get();
     }
 }
